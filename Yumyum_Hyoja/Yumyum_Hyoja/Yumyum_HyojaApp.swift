9731fbc51478709752868906917357b654a7c1ef
GOGO987 <goun987@naver.com>
2024-06-17 01:38:32 +0900

    Yumyum_Hyoja  생성

---
+++ Yumyum_Hyoja/Yumyum_Hyoja/Yumyum_HyojaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yumyum_HyojaApp.swift
+//  Yumyum_Hyoja
+//
+//  Created by 박고운 on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Yumyum_HyojaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
