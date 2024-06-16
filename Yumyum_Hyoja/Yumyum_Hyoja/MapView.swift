67401bc614c1b32104e763eb520b1b5a7fd717c6
GOGO987 <goun987@naver.com>
2024-06-17 02:14:51 +0900

    MapView 테스트

--- Yumyum_Hyoja/Yumyum_Hyoja/MapView.swift
+++ Yumyum_Hyoja/Yumyum_Hyoja/MapView.swift
@@ -5,4 +5,17 @@
 //  Created by 박고운 on 6/17/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        VStack {
+            Text("Map")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MapView()
+}
