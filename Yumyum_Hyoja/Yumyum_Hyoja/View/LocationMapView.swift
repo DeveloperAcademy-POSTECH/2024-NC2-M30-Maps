101d7d6273093906be54557bd7b8a4b3992dfd76
GOGO987 <goun987@naver.com>
2024-06-18 20:13:39 +0900

    LocationmapView추가

---
+++ Yumyum_Hyoja/Yumyum_Hyoja/View/LocationMapView.swift
@@ -0,0 +1,33 @@
+//
+//  LocationMapView.swift
+//  Yumyum_Hyoja
+//
+//  Created by 박고운 on 6/17/24.
+//
+
+// https://velog.io/@j_aion/SwiftUI-UberClone-MapView
+// https://codekodo.tistory.com/210
+
+import SwiftUI
+import MapKit
+
+struct LocationMapView: View {
+   
+    var body: some View {
+        Map()
+            }
+        }
+    
+
+
+
+//struct LocationMapView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        LocationMapView()
+//    }
+//}
+//
+//
+////#Preview {
+////    LocationMapView()
+////}
