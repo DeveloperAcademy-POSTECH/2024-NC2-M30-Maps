918f56cebab709be2c9c7c28188cc6ed0796278e
GOGO987 <goun987@naver.com>
2024-06-20 02:03:16 +0900

    호랭이가 감으로 바뀌는 것 수정

--- Yumyum_Hyoja/Yumyum_Hyoja/View/RandomView.swift
+++ Yumyum_Hyoja/Yumyum_Hyoja/View/RandomView.swift
@@ -1,42 +1,27 @@
-//
-//  RandomView.swift
-//  Yumyum_Hyoja
-//
-//  Created by 박고운 on 6/17/24.
-//
-
 import SwiftUI
 import CoreLocation
 
 struct RandomView: View {
     var coordinate: CLLocationCoordinate2D?
-    // coordinate 변수는 선택된 좌표를 저장
+    
     @State private var selectedCard: (name: String, view: AnyView, location: CLLocationCoordinate2D)?
-    // selectedCard: 선택된 카드의 이름, 뷰, 위치를 저장
     @State private var showCard = false
-    // showCard: 카드 표시 여부
     @State private var showMapView = false
-    // showMapView: 지도 뷰 표시 여부
     @State private var showLottieView = true
-    // showLottieView: Lottie 애니메이션 표시 여부, 초기 로딩 시에도 LottieView를 표시
     @State private var navigateToMapView = false
-    // navigateToMapView: 지도 뷰로의 네비게이션 여부
     @State private var lottieFilename = "middle"
-    // lottieFilename: 현재 보여지는 Lottie 애니메이션 파일명
     
     var body: some View {
         ZStack {
-            Color.main // 메인 색상을 전체 화면 배경색으로 설정
-                .edgesIgnoringSafeArea(.all) // Safe Area를 무시하고 전체 화면을 채우도록 설정
+            Color.main
+                .edgesIgnoringSafeArea(.all)
             
             VStack {
                 if showLottieView {
                     LottieView(filename: lottieFilename)
-                        // showLottieView가 true일 때 LottieView를 표시
                         .edgesIgnoringSafeArea(.all)
                         .onAppear {
                             DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                                // Lottie 애니메이션이 2초 동안 재생된 후 showLottieView를 false로 설정하고, 랜덤 카드를 선택하여 표시
                                 withAnimation {
                                     showLottieView = false
                                     selectRandomCard()
@@ -47,16 +32,13 @@ struct RandomView: View {
                 } else {
                     if let selectedCard = selectedCard {
                         RandomCardView(destinationCoordinate: selectedCard.location)
-                            //  showLottieView가 false일 때 선택된 카드가 있으면 이를 표시
-                            // RandomCardView는 선택된 카드의 위치
                             .padding()
                         
                         HStack {
                             Button(action: {
-                                showLottieView = true
-                                lottieFilename = "middle" // 초기 Lottie 애니메이션 파일로 변경
-                                // Lottie 애니메이션을 다시 표시, 카드 표시를 숨김
                                 withAnimation {
+                                    showLottieView = true
+                                    lottieFilename = "middle"
                                     showCard = false
                                 }
                             }) {
@@ -71,9 +53,9 @@ struct RandomView: View {
                             .padding()
                             
                             Button(action: {
-                                lottieFilename = "runhyo" // "runho" Lottie 애니메이션 파일로 변경
+                                showLottieView = true
+                                lottieFilename = "runhyo"
                                 withAnimation {
-                                    showLottieView = true
                                     DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
                                         navigateToMapView = true
                                     }
@@ -101,47 +83,30 @@ struct RandomView: View {
                 if newValue {
                     selectRandomCard()
                 }
-                // onChange(of:showCard)는 showCard가 변경될 때 랜덤 카드를 다시 선택
             }
-            
             .navigationBarTitle("", displayMode: .inline)
-            // 네비게이션 바 제목 숨기기
             .navigationBarBackButtonHidden(true)
-            // "< Back" 버튼 숨기기
             .onAppear {
-                // 초기 로딩 시 LottieView를 표시한 후 랜덤 카드 선택
                 DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                    // onAppear는 뷰가 나타날 때 Lottie 애니메이션을 표시하고, 2초 후 랜덤 카드를 선택하여 표시
                     withAnimation {
                         showLottieView = false
                         selectRandomCard()
                         showCard = true
                     }
                 }
-                
             }
-            
         }
     }
     
     private func selectRandomCard() {
-        // selectRandomCard() 메서드는 카드 배열에서 무작위로 하나의 카드를 선택
         let cards = [
             ("순이", CLLocationCoordinate2D(latitude: 36.0079529582591, longitude: 129.329425672387)),
             ("홍운반점", CLLocationCoordinate2D(latitude: 36.0079529582591, longitude: 129.329425672387)),
             ("덕수파스타", CLLocationCoordinate2D(latitude: 36.0079529582591, longitude: 129.329425672387)),
             ("김고미김밥", CLLocationCoordinate2D(latitude: 36.0079529582591, longitude: 129.329425672387))
         ]
-        // 카드 배열은 각각의 카드 이름과 위치를 포함
         if let card = cards.randomElement() {
             selectedCard = (card.0, AnyView(RandomCardView(destinationCoordinate: card.1)), card.1)
-            // randomElement() 메서드를 사용하여 무작위로 카드를 선택하고, selectedCard에 저장
         }
     }
 }
-
-//struct RandomView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        RandomView(coordinate: CLLocationCoordinate2D(latitude: 36.0079529582591, longitude: 129.329425672387))
-//    }
-//}
