dc0b45f936b5ad9c3212f1af526bc0b3ef7f04ba
JJaJan <167400158+ChanyuLee@users.noreply.github.com>
2024-06-19 23:31:31 +0900

    랜덤뷰 파일럿 추가

---
+++ Yumyum_Hyoja/Yumyum_Hyoja/View/RandomcardViewPilot.swift
@@ -0,0 +1,18 @@
+//
+//  RandomcardViewPilot.swift
+//  Yumyum_Hyoja
+//
+//  Created by 이찬유 on 6/19/24.
+//
+
+import SwiftUI
+
+struct RandomcardViewPilot: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RandomcardViewPilot()
+}
